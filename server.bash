58ba89e66e3a37f9961919c7ceff2d8e3aa6deec
Esysteme <aurelien.lequoy@gmail.com>
2013-09-25 11:21:39 +0200

    Update server.bash

--- server.bash
+++ server.bash
@@ -2,29 +2,23 @@ apt-get install -y git
 apt-get install -y vim
 
 
-#get install version of Debian
-#version = lsb_release -c | cut -d ':' -f2 | tr -d '\t'
-#version = lsb_release -sc
-
-#if [ $version = "wheezy" ]
-#then
+# install dotdeb 
 echo -e "deb http://packages.dotdeb.org wheezy-php55 all\n" >> /etc/apt/sources.list
 echo -e "deb-src http://packages.dotdeb.org wheezy-php55 all\n" >> /etc/apt/sources.list
-#else
-#echo -e "deb http://packages.dotdeb.org squeeze-php54 all\n" >> /etc/apt/sources.list
-#echo -e "deb-src http://packages.dotdeb.org squeeze-php54 all\n" >> /etc/apt/sources.list
-#fi
 
 cd /root
 wget http://www.dotdeb.org/dotdeb.gpg
 cat dotdeb.gpg | apt-key add -
 
-
 apt-get update
 apt-get -y upgrade
 
+
+#install screen
 apt-get install -y screen
 
+
+#install LAMP
 apt-get install -y apache2
 apt-get install -y php5
 
@@ -35,6 +29,7 @@ apt-get install -y curl
 apt-get install -y php5-curl php5-gd php5-mcrypt php5-mysqlnd
 
 
+#install tools
 apt-get install -y tree iftop nmap
 
 #mkdir -p /home/www/
