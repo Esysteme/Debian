2c5b31cbed42c31af98cbddf79e768add8b93ade
Esysteme <aurelien.lequoy@gmail.com>
2014-04-10 17:03:29 +0200

    Update mariadb10_on_ubuntu12_04.sh

--- mariadb10_on_ubuntu12_04.sh
+++ mariadb10_on_ubuntu12_04.sh
@@ -1,4 +1,3 @@
-
 apt-get -y install python-software-properties
 
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
@@ -12,3 +11,12 @@ apt-get update
 apt-get -y install mariadb-server
 
 #set password
+
+
+mkdir -p /var/lib/log_mysql
+
+chown mysql:mysql /var/lib/log_mysql
+
+
+mkdir -p /var/lib/mysql_backup
+chown mysql:mysql /var/lib/mysql_backup
