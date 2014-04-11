06e7a9407306570d935d4836a1fa978264f2f07d
Esysteme <aurelien.lequoy@gmail.com>
2014-04-11 10:55:40 +0200

    Update mariadb10_on_ubuntu12_04.sh

--- mariadb10_on_ubuntu12_04.sh
+++ mariadb10_on_ubuntu12_04.sh
@@ -1,6 +1,11 @@
 #to start this script :
 # curl -sS https://raw.githubusercontent.com/Esysteme/Debian/master/mariadb10_on_ubuntu12_04.sh | bash
 
+
+#mkfs.ext4 -j -L varlib -O dir_index -m 2 -J size=400 -b 4096 -R stride=16 /dev/sdb1
+
+
+
 apt-get -y install python-software-properties
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 echo "deb http://mirror.stshosting.co.uk/mariadb/repo/10.0/ubuntu precise main" >> /etc/apt/sources.list.d/mysql.list
