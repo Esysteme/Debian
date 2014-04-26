b7e8e911cbbfc1c7e07507c9ba0b70a578b6f7f1
Esysteme <aurelien.lequoy@gmail.com>
2014-04-26 18:22:12 +0200

    Update install_new.bash

--- install_new.bash
+++ install_new.bash
@@ -1,5 +1,21 @@
 sed -i 's/deb cdrom/#deb cdrom/g' /etc/apt/sources.list
 
-
 echo -e "deb http://packages.dotdeb.org wheezy-php55 all\n" >> /etc/apt/sources.list.d/php.list
 echo -e "deb-src http://packages.dotdeb.org wheezy-php55 all\n" >> /etc/apt/sources.list.d/php.list
+
+
+cd /root
+wget http://www.dotdeb.org/dotdeb.gpg
+cat dotdeb.gpg | apt-key add -
+rm dotdeb.gpg
+
+apt-get -y update
+apt-get -y upgrade
+
+apt-get -y install php5
+
+
+curl -sS https://getcomposer.org/installer | php
+
+
+mv composer.phar /usr/local/bin/composer
