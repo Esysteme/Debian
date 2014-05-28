faa7151e490f18ed6a141aec7f049dfc1017332f
Esysteme <aurelien.lequoy@gmail.com>
2014-05-28 11:40:54 +0200

    Update mariadb10_on_ubuntu12_04.sh

--- mariadb10_on_ubuntu12_04.sh
+++ mariadb10_on_ubuntu12_04.sh
@@ -32,14 +32,7 @@ chown mysql:mysql /data/mysql/data
 mkdir -p /data/mysql/binlog
 chown mysql:mysql /data/mysql/binlog
 
-testvm = `hostname | grep test | wc -l`
 
-if  testvm = 1 
-then
- innodb_use_native_aio=0
- else
- innodb_use_native_aio=1
-fi;
 
 cat >> /etc/mysql/conf.d/mariadb10.cnf << EOF
 
