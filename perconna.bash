93e812b2306ef77f73b9ada673f1d11ac2771817
Aurélien LEQUOY <aurelien.lequoy@gmail.com>
2014-08-01 17:42:50 +0200

    Update and rename xtrabackup.bash to perconna.bash

--- perconna.bash
+++ perconna.bash
@@ -1,4 +1,8 @@
 
+
+
+#curl -sS https://raw.githubusercontent.com/Esysteme/Debian/master/xtrabackup.bash | bash
+
 apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 
 gpg -a --export CD2EFD2A | apt-key add -
@@ -7,3 +11,5 @@ echo 'deb http://repo.percona.com/apt precise main' > /etc/apt/sources.list.d/pe
 echo 'deb-src http://repo.percona.com/apt precise main' > /etc/apt/sources.list.d/percona.list
 
 apt-get update
+
+
