37574733a0b12a0905daaec2454c6d030123e742
Aurélien LEQUOY <aurelien.lequoy@gmail.com>
2014-08-01 17:44:03 +0200

    Update perconna.bash

--- perconna.bash
+++ perconna.bash
@@ -1,6 +1,3 @@
-
-
-
 #curl -sS https://raw.githubusercontent.com/Esysteme/Debian/master/xtrabackup.bash | bash
 
 apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
@@ -11,5 +8,3 @@ echo 'deb http://repo.percona.com/apt precise main' > /etc/apt/sources.list.d/pe
 echo 'deb-src http://repo.percona.com/apt precise main' > /etc/apt/sources.list.d/percona.list
 
 apt-get update
-
-
