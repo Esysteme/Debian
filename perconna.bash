5c4f0284f564b4f9f3fb9fefea79442829d4d622
Aurélien LEQUOY <aurelien.lequoy@gmail.com>
2014-08-01 17:45:03 +0200

    Update perconna.bash

--- perconna.bash
+++ perconna.bash
@@ -5,6 +5,6 @@ apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 gpg -a --export CD2EFD2A | apt-key add -
 
 echo 'deb http://repo.percona.com/apt precise main' > /etc/apt/sources.list.d/percona.list
-echo 'deb-src http://repo.percona.com/apt precise main' > /etc/apt/sources.list.d/percona.list
+echo 'deb-src http://repo.percona.com/apt precise main' >> /etc/apt/sources.list.d/percona.list
 
 apt-get update
