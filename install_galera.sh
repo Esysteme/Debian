48b4043ed8b9d90ddb4b55b7866f108cdbff8f05
Aurélien LEQUOY <aurelien.lequoy@gmail.com>
2015-02-05 11:42:39 +0100

    Update install_galera.sh

--- install_galera.sh
+++ install_galera.sh
@@ -3,30 +3,27 @@
 apt-get update
 apt-get -y upgrade
 
-
-
 apt-get -y install python-software-properties
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
  
-release=`lsb_release -rs`
-
+release=`lsb_release -cs`
 
 echo "Version : "$release
 
-if [ "$release" = "14.04" ]
-then
-echo "Version : "$release
- echo "deb http://mirror.stshosting.co.uk/mariadb/repo/10.0/ubuntu trusty main" > /etc/apt/sources.list.d/mysql.list
- echo "deb-src http://mirror.stshosting.co.uk/mariadb/repo/10.0/ubuntu trusty main" >> /etc/apt/sources.list.d/mysql.list
-elif [ $release = "12.04" ]
-then
-echo "Version : "$release
- echo "deb http://mirrors.coreix.net/mariadb/repo/10.0/ubuntu precise main" > /etc/apt/sources.list.d/mysql.list
- echo "deb-src http://mirrors.coreix.net/mariadb/repo/10.0/ubuntu precise main" >> /etc/apt/sources.list.d/mysql.list
-else
-echo "version not supported"
-exit 1
-fi
- 
+echo "deb http://mirror.stshosting.co.uk/mariadb/repo/10.0/ubuntu $release main" > /etc/apt/sources.list.d/mysql.list
+echo "deb-src http://mirror.stshosting.co.uk/mariadb/repo/10.0/ubuntu $release main" >> /etc/apt/sources.list.d/mysql.list
+
 apt-get update
 apt-get -y install mariadb-galera-server galera rsync
+
+
+
+apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+
+ 
+echo "deb http://repo.percona.com/apt $release main" > /etc/apt/sources.list.d/percona.list
+echo "deb-src http://repo.percona.com/apt $release main" >> /etc/apt/sources.list.d/percona.list
+ 
+apt-get update
+ 
+apt-get -y install xtrabackup percona-toolkit
