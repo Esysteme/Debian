c7a20f99e51382017ab4ef3775179bfbe06f692c
Aurélien LEQUOY <aurelien.lequoy@gmail.com>
2015-02-05 11:07:24 +0100

    Create install_galera.sh

---
+++ install_galera.sh
@@ -0,0 +1,22 @@
+apt-get -y install python-software-properties
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+ 
+ 
+if [ $release = "14.04" ]
+then
+echo "Version : "$release
+ echo "deb http://mirror.stshosting.co.uk/mariadb/repo/10.0/ubuntu trusty main" > /etc/apt/sources.list.d/mysql.list
+ echo "deb-src http://mirror.stshosting.co.uk/mariadb/repo/10.0/ubuntu trusty main" >> /etc/apt/sources.list.d/mysql.list
+elif [ $release = "12.04" ]
+then
+echo "Version : "$release
+ echo "deb http://mirrors.coreix.net/mariadb/repo/10.0/ubuntu precise main" > /etc/apt/sources.list.d/mysql.list
+ echo "deb-src http://mirrors.coreix.net/mariadb/repo/10.0/ubuntu precise main" >> /etc/apt/sources.list.d/mysql.list
+else
+echo "version not supported"
+exit 1
+fi
+ 
+apt-get update
+ 
+apt-get -y install mariadb-galera-server galera rsync
